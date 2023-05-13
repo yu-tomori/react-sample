0f3a30c001452fefea0cdbc428a4286edba375ef
yu <peacetomori1118@gmail.com>
2023-05-13 17:47:11 +0900

    do not required

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,7 @@ import Header from './components/Header';
 const App: FC = () => {
   return (
     <div className="App">
-      <Header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </Header>
+      <Header className="App-header">header</Header>
       <Layout>
         <div>content</div>
       </Layout>
